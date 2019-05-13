d0c61dc067ce66876e6890ee7c6a4926a05bffa6
Neil Eckstein <49618092+neckstein@users.noreply.github.com>
2019-05-13 14:32:40 -0500

    Design Revision after additional Research

--- designs.js
+++ designs.js
@@ -1,33 +1,35 @@
-//Define variables
-const pixelC = document.getElementById('pixelCanvas');
-const colorP = document.getElementById('colorPicker');
-const sizeP = document.getElementById('sizePicker');
-const width = document.getElementById('inputWidth');
-const height = document.getElementById('inputHeight');
-//Create tr and td element
-const row = document.createElement('tr')
-const col = document.createElement('td')
-//AddEventListener for Submit Button
-sizeP.addEventListener('submit', function (event){
-  event.preventDefault()
-  makeGrid()
-});
+//Get canvas, color, and size element Ids
+var pixelC = document.getElementById('pixelCanvas');
+var colorP = document.getElementById('colorPicker').value;
+var sizeP = document.getElementById('sizePicker');
 
 function makeGrid() {
+  //Get height and width element Ids
+  const width = document.getElementById('inputWidth').value;
+  const height = document.getElementById('inputHeight').value;
+  //Clear Grid
+  pixelC.innerHTML = null;
   //Create nested loop to create rows then columns to create grid
-  for (let r = 0; r < width.value; r++){
-    //Create tr elements based on width value
-    pixelC.appendChild(row)
-    for (let c = 0; c < height.value; c++){
-      //Create td elements based on height value
-      row.appendChild(col)
-
-      //AddEventListener to apply color value to a cell
-      colorP.addEventListener('color', function(event){
-        event.style.backgroundColor = colorP.value;
+  for (let r = 1; r <= height; r++){
+    //Create tr element
+    const row = document.createElement('tr');
+    //Insert tr element into table createElement
+    pixelC.appendChild(row);
+    for (let c = 1; c <= width; c++){
+      //Create td elements
+      const cell = document.createElement('td');
+      //Insert td element into tr element
+      row.appendChild(cell);
+      //Create funtion to set background Color (color) pixels
+      cell.addEventLister('mousedown', function(){
+        this.style.backgroundColor = colorP
       });
     }
   }
 }
 
-makeGrid();
+//AddEventListener for Submit Button
+sizeP.addEventListener('click', function (event){
+  event.preventDefault();
+  makeGrid();
+});
